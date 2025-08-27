a3e88b3f2f90153ea9523733d7b20c4afffd9592
robertoe135 <63330625+robertoe135@users.noreply.github.com>
2025-08-27 15:25:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,6 @@ st.markdown("""
 This app allows you to:
 1. Download a blank CSV template
 2. Upload a filled CSV to generate a `fixtureLibrary.xml` file
-
-To 'install' the generated fixture library, you need to drag and drop the generated file to: C:\Users\[USERNAME]\AppData\Roaming\COMcheck and replace the existing file!
 """)
 
 # Step 1: Downloadable empty CSV template
